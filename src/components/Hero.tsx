58e1c82ed1d88320ff7f593e2e414f0e7ade81ce
PawelRudko <61045167+PawelRudko@users.noreply.github.com>
2026-04-16 19:46:57 +0200

    Zmiana zdjecia hero na Ferrari

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
-import heroImg from "@/assets/hero-f1.jpg";
+import heroImg from "@/assets/Hero__1.jpg";
 
 const Hero = () => {
   const ref = useRef<HTMLDivElement>(null);
